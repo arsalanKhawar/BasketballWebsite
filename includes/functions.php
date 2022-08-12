11c4613b37647d9826cbb3f120c88419adb21804
arsalanKhawar <ak78@njit.edu>
2022-08-12 00:24:03 -0400

    added login page, navigation page, and function page

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'includes/dbh.php';
+
+function is_logged_in($redirect = false, $destination = "login.php")
+{
+    $isLoggedIn = isset($_SESSION["user"]);
+    if ($redirect && !$isLoggedIn) {
+        echo "You must be logged in to view this page";
+        die(header("Location: $destination"));
+    }
+    return $isLoggedIn; //se($_SESSION, "user", false, false);
+}
+?>
