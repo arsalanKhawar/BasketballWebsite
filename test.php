f92e88a2ceda3525a3c9b25b686bb12b9d695b9a
arsalanKhawar <ak78@njit.edu>
2022-08-11 23:51:29 -0400

    updated dbh.php to create connections. Also added personal notes in test file.

--- test.php
+++ test.php
@@ -1,9 +1,6 @@
 <?php
 include_once 'includes/dbh.php';
 
-
-$username = "arsalan";
-$password = "hello123";
 ?>
 
 <html lang="en">
@@ -15,5 +12,57 @@ $password = "hello123";
 </head>
 <body>
     hello World, My name is Arsalan.
+    <br>
+
+    
+    <?php
+        /*
+        //how to insert data into a table
+        $sql = "INSERT INTO users (email,password) VALUES ('$email','$password');";
+        $result = mysqli_query($conn, $sql);
+        */
+        //how to insert data into a table method 2
+        $myemail = "new@gmail.com";
+        $password = "pswd";
+        $sql = "INSERT INTO users (email,password) VALUES (?,?);";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            echo "sql error";
+        }
+        else{
+            mysqli_stmt_bind_param($stmt,"ss", $myemail, $password);
+            mysqli_stmt_execute($stmt);
+        }
+/*
+        //how to get data from table, method 1
+        $sql = "SELECT * FROM users ;";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['email'] . "<br>";
+            }
+        }
+        else{
+            echo "failed to get from database";
+        }
+*/
+        //how to get data from table, method 2
+        $myemail = "ars@gmail.com";
+        $sql = "SELECT * FROM users WHERE email =? ;";
+        $stmt = mysqli_stmt_init($conn,);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            echo "Sql fail";
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "s", $myemail);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['email'] . "<br>";
+            }
+        }
+            
+    ?>
 </body>
 </html>
