680f537a203dede38dc316f4a84a6eae19d6bf28
arsalanKhawar <ak78@njit.edu>
2022-08-12 22:14:00 -0400

    Added a css style sheet, and added a basic navigation bar

---
+++ nav.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'includes/functions.php';
+?>
+<link rel="stylesheet" href="<?php echo 'styles.css'; ?>">
+
+<nav>
+    <ul>
+        <?php if(!is_logged_in()) : ?>
+            <li><a href = "<?php echo 'login.php' ?>">Login</a></li>
+            <li><a href = "<?php echo 'register.php' ?>">Register</a></li>
+            <li><a href = "<?php echo 'about.php' ?>">About</a></li>
+            <?php endif;?>
+    </ul>
+</nav>
