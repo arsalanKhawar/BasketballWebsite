c556cae2b22339207445d4d6ee25181929c35e95
arsalanKhawar <ak78@njit.edu>
2022-08-29 12:46:33 -0400

    added a form in addteam.php to collect team data. added addteam.inc.php to recieve data

--- addteam.php
+++ addteam.php
@@ -1,4 +1,17 @@
+<!--This page will allow the user to add a team. -->
 <?php
 include_once 'includes/functions.php';
 include_once 'nav.php';
 ?>
+
+<link rel="stylesheet" href="<?php echo 'styles.css'; ?>">
+
+<section class=addteam>
+    <h2 id=addteamheader>Add a team</h2>
+    <div class="addteamform">
+        <form action="includes/addteam.inc.php" method="POST">
+            <input type="text" name="teamname" placeholder="Team Name"> 
+            <button type="submit" name="submit" >Add Team</button>
+        </form>
+    </div>
+</section>
