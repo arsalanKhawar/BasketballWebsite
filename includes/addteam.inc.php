4db77989969f583549c1e5ef5a2dc92bed2d64fc
arsalanKhawar <ak78@njit.edu>
2022-08-29 13:43:05 -0400

    added functions in addteam.inc.php. Defined one of the functions in functions.php

---
+++ includes/addteam.inc.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST["submit"])){
+    $teamname = $_POST["teamname"];
+    require_once "dbh.php";
+    require_once "functions.php";
+
+    if(emptyteamname($teamname) !== false){
+        header("location: ../addteam.php?error=emptyteamname");
+        exit();
+    }
+    if(teamnameExists($conn, $teamname) !== false){
+        header("location: ../addteam.php?error=teamnameExists");
+        exit();
+    }
+    addteam($teamname);
+}
+?>
